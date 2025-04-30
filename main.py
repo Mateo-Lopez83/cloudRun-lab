ea013269037944188bac21647a0b2571c16926c7
MATEO_LOPEZ <m.lopez112@uniandes.edu.co>
2025-04-29 20:45:19 -0500

    cambios main

--- main.py
+++ main.py
@@ -22,10 +22,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    """Example Hello World route."""
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
-
+    """Ejemplo para el commit."""
+    name = os.environ.get("NAME", "Experto en Google Cloud")
+    return f"Hola {name}!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
